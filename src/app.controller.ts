6905611f595586f9248c51d6957443718fe10884
christian <cristhho@gmail.com>
2021-05-07 11:22:09 -0500

    get method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,32 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 10,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit: ${limit}, offset: ${offset}, brand: ${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return 'this is the filter';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product ${productId}`;
+  }
+
+  @Get('categories/:categoryId/products/:productId')
+  getCategoryProduct(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Product ${productId} in category ${categoryId}`;
   }
 }
