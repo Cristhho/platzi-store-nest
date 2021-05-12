36c9df5350935d88a4d752774f7fcf719649f2af
christian <cristhho@gmail.com>
2021-05-12 12:09:52 -0500

    avoid save non expected params

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      //forbidNonWhitelisted: true, Activate if you want to show errors message associated with the non expected param
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
